267426456d028d7d3e40b6343ff261dad37b7693
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-04-01 14:53:11 +0200

    Create dummy service implementation

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,37 @@
+import { Tasks } from './interfaces/tasks.interface';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+
+    // Dummy data ( from db en production )
+    tasks: Tasks[] = [
+        {
+            id: 1,
+            title: 'Task one',
+            description: 'the task one',
+            done: true,
+        },
+        {
+            id: 2,
+            title: 'Task two',
+            description: 'the task two',
+            done: true,
+        },
+        {
+            id: 3,
+            title: 'Task three',
+            description: 'the task three',
+            done: false,
+        },
+    ];
+
+    getTesks() {
+        return this.tasks;
+    }
+
+    getTask(id: number) {
+        return this.tasks.find( task => task.id === id );
+    }
+
+}
