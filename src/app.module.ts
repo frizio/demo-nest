97e9daea7c32937e4ecc4024c0d2ac8a45a62945
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-04-01 14:37:12 +0200

    nest g module tasks

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksController } from './tasks/tasks.controller';
 import { TasksService } from './tasks/tasks.service';
+import { TasksModule } from './tasks/tasks.module';
 
 // Modulo principale
 // Modulo: contenitore di una parte dell'applicazione che racchiude una distinta funzionalità
 // Modulo = Controller + Servizi
 
 @Module({
-  imports: [],
+  imports: [TasksModule],
   controllers: [AppController, TasksController],
   providers: [AppService, TasksService],
 })
