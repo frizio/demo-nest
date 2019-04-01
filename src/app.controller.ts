f11c759a07da66946a83533a1ca6ae9c6f5defbf
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-04-01 12:26:31 +0200

    Controller description

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// Controller: Paths dell'applicazione
+// Questo controller controlla la home /
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
