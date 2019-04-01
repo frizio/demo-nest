a229e982d08f1dfb365f8f269fdc066f7360650d
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-04-01 14:40:26 +0200

    Define basic module structure

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,4 +1,10 @@
+import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
 import { Module } from '@nestjs/common';
 
-@Module({})
+@Module({
+    imports: [],
+    controllers: [TasksController],
+    providers: [TasksService]
+})
 export class TasksModule {}
