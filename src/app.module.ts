cda2d3ab778cc6d8e6524108e97d30182e126c43
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-04-01 12:26:22 +0200

    Module description

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// Modulo principale
+// Modulo: contenitore di una parte dell'applicazione che racchiude una distinta funzionalità
+// Modulo = Controller + Servizi
+
 @Module({
   imports: [],
   controllers: [AppController],
