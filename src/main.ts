26532478cf5724833d3e3bab3b875df0d67d86b3
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-04-01 12:16:37 +0200

    main.ts description

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// File principale che avvia dell'applicazione
+// NestFactory è il servizio che avvia l'applicazione
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+
 bootstrap();
