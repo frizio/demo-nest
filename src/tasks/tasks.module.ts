959fbb6261e66245fb33d21263b8411c554e2c1e
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-04-03 15:46:55 +0200

    Import schema inside a module

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,15 @@
+import { TaskSchema } from './schemas/task.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { Module } from '@nestjs/common';
 
 @Module({
-    imports: [],
+    imports: [ MongooseModule.forFeature(
+        [
+            {name: 'Task', schema: TaskSchema },
+        ]
+    ) ],
     controllers: [TasksController],
     providers: [TasksService],
 })
