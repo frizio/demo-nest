9c3cc7d235cbc19a8cc2d50672cff5ecc92ae871
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-04-03 15:33:37 +0200

    Connect to the mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,14 @@ import { AppService } from './app.service';
 import { TasksController } from './tasks/tasks.controller';
 import { TasksService } from './tasks/tasks.service';
 import { TasksModule } from './tasks/tasks.module';
+import { MongooseModule } from "@nestjs/mongoose";
 
 // Modulo principale
 // Modulo: contenitore di una parte dell'applicazione che racchiude una distinta funzionalità
 // Modulo = Controller + Servizi
 
 @Module({
-  imports: [TasksModule],
+  imports: [ TasksModule, MongooseModule.forRoot('mongodb://user:password@localhost:27017/test', {useNewUrlParser: true} ) ],
   controllers: [AppController, TasksController],
   providers: [AppService, TasksService],
 })
